07f36f772898246691310e8b3a829c2ead48ddf7
Hosang Yoon <junosan@users.noreply.github.com>
2016-10-13 15:02:42 +0900

    Add Reshaper for policy net

---
+++ core/rnn/policy/PolicyDataSet.h
@@ -0,0 +1,10 @@
+/* ========================================================================== */
+/*  Copyright (C) 2016 Hosang Yoon (hosangy@gmail.com) - All Rights Reserved  */
+/*  Unauthorized copying of this file, via any medium is strictly prohibited  */
+/*                        Proprietary and confidential                        */
+/* ========================================================================== */
+
+#ifndef POLICYDATASET_H_
+#define POLICYDATASET_H_
+
+#endif /* POLICYDATASET_H_ */
---
+++ core/rnn/policy/PolicyNet.h
@@ -0,0 +1,10 @@
+/* ========================================================================== */
+/*  Copyright (C) 2016 Hosang Yoon (hosangy@gmail.com) - All Rights Reserved  */
+/*  Unauthorized copying of this file, via any medium is strictly prohibited  */
+/*                        Proprietary and confidential                        */
+/* ========================================================================== */
+
+#ifndef POLICYNET_H_
+#define POLICYNET_H_
+
+#endif /* POLICYNET_H_ */
